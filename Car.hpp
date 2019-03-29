084093b4b063f52a4caaa880caf6d95938a031d0
Brannon Centeno <bccenten@eagle.fgcu.edu>
2019-03-29 19:43:53 -0400

    Added some more stuff to the lambda function examples and cleaned up the code a bit

--- Car.hpp
+++ Car.hpp
@@ -2,22 +2,20 @@
 #include <sstream>
 #include <iostream>
 
-using namespace std;
-
 class Car
 {
   public:
     Car(/* Default Constructor */) : make(""), model(""), topSpeedMPH(0) {}
-    Car(const string &inMake, const string &inModel, const int inTopSpeedMPH) : make(inMake), model(inModel), topSpeedMPH(inTopSpeedMPH) {}
+    Car(const std::string &inMake, const std::string &inModel, const int inTopSpeedMPH) : make(inMake), model(inModel), topSpeedMPH(inTopSpeedMPH) {}
     ~Car() {}
 
-    string to_string()
+    std::string to_string()
     {
-        stringstream message;
+        std::stringstream message;
         message << "Make: " << make << ", Model: " << model << ", Speed: " << topSpeedMPH;
         return message.str();
     }
 
-    string make, model;
+    std::string make, model;
     int topSpeedMPH;
 };
--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <functional> // Include to use lambda expressions
+#include <math.h>
 
 #include "Dog.hpp"
 #include "Car.hpp"
-
-using namespace std;
 
 void LambdaFunctionExamples();
 
 int main()
 {
-
-    LambdaFunctionExamples();
+    // LambdaFunctionExamples();
 
     return 0;
 }
@@ -27,36 +26,55 @@ void LambdaFunctionExamples()
     // Negates polluting a class with a function that's repeatedly called
     // in only one function.
 
-    // Syntax [captures](parameters) -> optionalReturnType { statements }
+    // Fun fact: Lambda-calculus was the first object-oriented
+    // language (1932) - William Cook, "On Understanding Data Abstraction, Revisited"
+
+    // Syntax: [](){} 
+    // [] capture
+    // () arguments (params)
+    // {} lambda implementation (function meat)
 
     /**********************************************************/
     /************************* Basics *************************/
     /**********************************************************/
 
-    // a simple lambda
-    function<int(int, int)> add = [](int inFirstParam, int inSecondParam) {
+    // This is a valid lambda expression!
+    // [](){}; /* Commented out to prevent compiler warnings */
+
+    // a simple lambda (that automatically calls itself)
+    [](int val){
+        std::cout << val << std::endl << std::endl;
+    }(100);
+
+    // Lambda functions need not be called in-place. In almost all cases
+    // it's better store them in a function pointer so they can be called
+    // wherever. New are C++11 style function pointers. No need to make typdefs
+    // or use native syntax
+    std::function<int(int, int)> add = [](int inFirstParam, int inSecondParam) {
         return inFirstParam + inSecondParam;
     };
 
-    cout << "1 + 2 = " << add(1, 2) << endl
-         << endl;
+    std::cout << "1 + 2 = " << add(1, 2) << std::endl;
 
-    // When creating custom lambda functions, it's way better to just use auto
-    // as the return type
+    // When creating custom lambda functions, 
     auto subtract = [](int inFirstParam, int inSecondParam) {
         return inFirstParam - inSecondParam;
     };
 
-    cout << "5 - 6 = " << subtract(5, 6) << endl
-         << endl;
+    std::vector<std::function<int(int, int)>> functionVector = {add, subtract};
+    for (auto aFunction : functionVector)
+    {
+        std::cout << aFunction(2, 3) << std::endl;
+    }
+    std::cout << "5 - 6 = " << subtract(5, 6) << std::endl;
 
     // You can optionally declare a return type
     auto truncatedMultiply = [](double inFirstParam, double inSecondParam) -> int {
         return inFirstParam * inSecondParam;
     };
 
-    cout << "5.5 * 6.3 = " << truncatedMultiply(5.5, 6.3) << endl
-         << endl;
+    std::cout << "5.5 * 6.3 = " << truncatedMultiply(5.5, 6.3) << std::endl
+         << std::endl;
 
     // Parameters can be made const and be passed by reference and behave as you would expect
 
@@ -65,21 +83,21 @@ void LambdaFunctionExamples()
     Car myCar("Ferrari", "458 Italia", 202);
 
     // Notice that the car is being altered via reference
-    auto carUpdate = [](Car &outCar, const string &inMake, const string &inModel, const int inTopSpeedMPH) {
+    auto carUpdate = [](Car &outCar, const std::string &inMake, const std::string &inModel, const int inTopSpeedMPH) {
         outCar.make        = inMake;
         outCar.model       = inModel;
         outCar.topSpeedMPH = inTopSpeedMPH;
     };
 
     // Before
-    cout << "Before Update:\n\t" << myCar.to_string() << endl
+    std::cout << "Before Update:\n\t" << myCar.to_string() << std::endl
          << std::endl;
 
     // Update the car via reference
     carUpdate(myCar, "Toyota", "Corolla", 90);
 
     // After
-    cout << "After Update:\n\t" << myCar.to_string() << endl
+    std::cout << "After Update:\n\t" << myCar.to_string() << std::endl
          << std::endl;
      
     /**********************************************************************/
@@ -110,7 +128,7 @@ void LambdaFunctionExamples()
     auto captureByValue = [=](/* No parameters passed */)
     {
         // This is a copy
-        std::cout << myString << std::endl << std::endl;
+        std::cout << myString << std::endl;
     };
     captureByValue();
 
@@ -121,7 +139,7 @@ void LambdaFunctionExamples()
     };
     captureByReference(); // Updates myString via reference
 
-    std::cout << myString << std::endl << std::endl;
+    std::cout << myString << std::endl;
 
     // Capture local variables by value
     std::string myString1("value 1"), myString2("value 2");
@@ -139,4 +157,33 @@ void LambdaFunctionExamples()
     };
     referenceCaptureListExample();
     std::cout << "Captured and altered via reference capture list: " << myString1 << " " << myString2 << std::endl << std::endl;
+
+    /**********************************************************/
+    /***************** Functional Programming *****************/
+    /**********************************************************/
+
+    // Functional programming is all about writing the program functions
+    // similar to a mathematical fucntion 
+    // ex. f(x) = x * x;
+    //     f(5) = 25
+    //     f(x, y) = x^2 + y^2
+    //     f(2, 5) = 29
+
+    // OO makes code understandable by encapsulating moving
+    // parts. Functional programming makes code understandable by minimizing
+    // moving parts. - Michael Feathers
+
+    // No matter what language you work in, programming in a functional style provides
+    // benefits. You should do it whenever it is convenienet and you should think hard
+    // about the decision when it isn't convenient - John Carmack
+
+    auto quadraticFormula = [](double a, double b, double c) -> std::pair<double, double> {
+        double x1 = [](double a, double b, double c){ return (-b + sqrt(pow(b, 2) - 4*a*c)) / (2 * a); }(a, b, c);
+        double x2 = [](double a, double b, double c){ return (-b - sqrt(pow(b, 2) - 4*a*c)) / (2 * a); }(a, b, c);
+        return std::make_pair(x1, x2);
+    };
+
+    // x^2 + x - 1
+    std::pair<double, double> result = quadraticFormula(1, 1, -1);
+    std::cout << result.first << ", " << result.second << std::endl;
 }
